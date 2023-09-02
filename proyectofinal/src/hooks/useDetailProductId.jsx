01a4ecff737b7333d94b112ed5fbc17ea4593e1a
alex_rb <alexisjrb2908@gmail.com>
2023-09-02 17:23:57 -0300

    firebase

--- proyectofinal/src/hooks/useDetailProductId.jsx
+++ proyectofinal/src/hooks/useDetailProductId.jsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getProductById } from "../asyncMock";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export const useDetailProductId = () => {
   const [product, setProduct] = useState(null);
 
   const { itemId } = useParams();
   useEffect(() => {
-    getProductById(Number(itemId))
-      .then((response) => setProduct(response))
-      .catch((error) => console.log(error));
+    const docRef = doc(db, "products", itemId);
+
+    getDoc(docRef)
+      .then((res) => {
+        const data = res.data();
+        const productAdapted = { id: res.id, ...data };
+        setProduct(productAdapted);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [itemId]);
 
   return { product };
--- proyectofinal/src/hooks/useGetProducts.jsx
+++ proyectofinal/src/hooks/useGetProducts.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
-import { getProductsByCategory, getProducts } from "../asyncMock";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export const useGetProducts = (categoryId) => {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    const asyncFunc = categoryId ? getProductsByCategory : getProducts;
-    asyncFunc(categoryId)
-      .then((response) => setProducts(response))
-      .catch((error) => console.log(error));
+    const collectionRef = categoryId
+      ? query(collection(db, "products"), where("category", "==", categoryId))
+      : collection(db, "products");
+
+    getDocs(collectionRef)
+      .then((res) => {
+        const productsAdapted = res.docs.map((doc) => {
+          const data = doc.data();
+          return { id: doc.id, ...data };
+        });
+        setProducts(productsAdapted);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [categoryId]);
 
   return { products };
